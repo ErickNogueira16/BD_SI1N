5a391a0890f928963a36bda56f811fca93f854b6
Erick Nogueira Alvarenga <117676267+ErickNogueira16@users.noreply.github.com>
2024-05-24 20:52:32 -0300

    Update aula1705.sql

--- aula1705.sql
+++ aula1705.sql
@@ -1,33 +1,36 @@
 -- Lista 17/05
 
+/*select * from pacientes*/
+
 /*select nome, email from pacientes
 where year(data_nasc) > 2000*/
 
 /*select m.nome, d.nome_especialidade from medicos m
-join departamentos d on m.depto_cod_espec = d.cod_especialidade*/
+left join departamentos d on m.depto_cod_espec = d.cod_especialidade*/
 
 /*select p.nome, m.nome, id_consulta, data_cons, hora_cons
 from consultas c join pacientes p on p.cpf_paciente = c.cpf_paciente
 join medicos m on m.crm = c.crm and m.uf = c.uf*/
 
 /*select p.nome, count(c.cpf_paciente) from consultas c
-join pacientes p on p.cpf_paciente = c.cpf_paciente
+right join pacientes p on p.cpf_paciente = c.cpf_paciente
 group by p.nome*/
 
-/*select d.nome_especialidade, c.data_cons from departamentos d
+/*select distinct d.nome_especialidade from departamentos d
 join medicos m on m.depto_cod_espec = d.cod_especialidade
 join consultas c on c.crm = m.crm and c.uf = m.uf
-where year(data_cons) = 2023;*/
+where year(data_cons) = 2024;*/
 
 /*select nome_especialidade from departamentos 
 order by nome_especialidade asc*/
 
-/*select nome, email from medicos where telefone = ''*/
+/*select nome, email from medicos where telefone = '' or telefone is null*/
 
 /*select p.nome from consultas c
 join pacientes p on p.cpf_paciente = c.cpf_paciente
 join medicos m on m.crm = c.crm and m.uf = c.uf
-where depto_cod_espec = 1*/
+join departamentos d on m.depto_cod_espec = d.cod_especialidade
+where nome_especialidade = 'cardiologia'*/
 
 /*SELECT d.nome_especialidade, AVG(c.cont) 
 FROM (
